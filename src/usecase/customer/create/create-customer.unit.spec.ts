af48731820dbffca8f21bc0efdeb7b9b7314b691
loxt <emannuelloxt@hotmail.com>
2022-04-28 23:26:58 -0400

    test(customer): should throw an error when street is missing

--- src/usecase/customer/create/create-customer.unit.spec.ts
+++ src/usecase/customer/create/create-customer.unit.spec.ts
@@ -37,4 +37,24 @@ describe('Unit test create customer use case', function () {
       }
     })
   });
+
+  it('should throw an error when name is missing', async function () {
+    const customerRepository = CustomerRepositoryMock()
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository)
+
+    input.name = '';
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow('Name is required');
+
+  });
+
+  it('should throw an error when street is missing', async function () {
+    const customerRepository = CustomerRepositoryMock()
+    const customerCreateUseCase = new CreateCustomerUseCase(customerRepository)
+
+    input.address.street = '';
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow('Street is required');
+
+  });
 });
