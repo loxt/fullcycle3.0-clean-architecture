c65d9e2f384898a39b2ded3dfda0abae8de455b5
loxt <emannuelloxt@hotmail.com>
2022-07-14 23:59:41 -0400

    test: should update a product

---
+++ src/usecase/product/update/update-product.unit.spec.ts
@@ -0,0 +1,30 @@
+import Product from '../../../domain/product/entity/product';
+import {v4 as uuid} from 'uuid';
+import UpdateProductUseCase from './update-product.usecase';
+
+const product = new Product(uuid(), 'Product 1', 100);
+
+const input = {
+  id: product.id,
+  name: product.name,
+  price: product.price,
+};
+
+const ProductRepositoryMock = () => {
+  return {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findById: jest.fn().mockReturnValue(Promise.resolve(product)),
+    update: jest.fn(),
+  };
+};
+
+describe('Unit test for product update use case', function() {
+  it('should update a product', async function() {
+    const productRepository = ProductRepositoryMock();
+    const productUpdateUseCase = new UpdateProductUseCase(productRepository);
+    const output = await productUpdateUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
---
+++ src/usecase/product/update/update-product.usecase.ts
@@ -0,0 +1,23 @@
+import {
+  InputUpdateProductDto,
+  OutputUpdateProductDto,
+} from './update-product.dto';
+import ProductRepositoryInterface from '../../../domain/product/repository/product-repository.interface';
+
+export default class UpdateProductUseCase {
+  constructor(private readonly productRepository: ProductRepositoryInterface) {}
+
+  async execute(input: InputUpdateProductDto): Promise<OutputUpdateProductDto> {
+    const product = await this.productRepository.findById(input.id);
+
+    product.changeName(input.name);
+    product.changePrice(input.price);
+    await this.productRepository.update(product);
+
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    };
+  }
+}
