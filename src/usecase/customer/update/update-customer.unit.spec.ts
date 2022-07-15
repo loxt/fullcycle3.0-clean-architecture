eaa041cc1278dde99e16e426382466fbf855c9b4
loxt <emannuelloxt@hotmail.com>
2022-07-15 12:19:11 -0400

    test(integration): should update a product

--- src/usecase/customer/update/update-customer.unit.spec.ts
+++ src/usecase/customer/update/update-customer.unit.spec.ts
@@ -1,8 +1,11 @@
-import CustomerFactory from "../../../domain/customer/factory/customer.factory";
-import Address from "../../../domain/customer/value-object/address";
-import UpdateCustomerUseCase from "./update-customer.usecase";
+import CustomerFactory from '../../../domain/customer/factory/customer.factory';
+import Address from '../../../domain/customer/value-object/address';
+import UpdateCustomerUseCase from './update-customer.usecase';
 
-const customer = CustomerFactory.createWithAddress('John', new Address('Street', 123, 'zip', 'city'))
+const customer = CustomerFactory.createWithAddress(
+    'John',
+    new Address('Street', 123, 'zip', 'city'),
+);
 
 const input = {
   id: customer.id,
@@ -11,9 +14,9 @@ const input = {
     street: 'Street Updated',
     number: 321,
     zip: 'zip updated',
-    city: 'city updated'
-  }
-}
+    city: 'city updated',
+  },
+};
 
 const CustomerRepositoryMock = () => {
   return {
@@ -21,16 +24,16 @@ const CustomerRepositoryMock = () => {
     findAll: jest.fn(),
     findById: jest.fn().mockReturnValue(Promise.resolve(customer)),
     update: jest.fn(),
-  }
-}
+  };
+};
 
-describe('Unit test for customer update use case', function () {
-  it('should update a customer', async function () {
-    const customerRepository = CustomerRepositoryMock()
-    const customerUpdateUseCase = new UpdateCustomerUseCase(customerRepository)
+describe('Unit test for customer update use case', function() {
+  it('should update a customer', async function() {
+    const customerRepository = CustomerRepositoryMock();
+    const customerUpdateUseCase = new UpdateCustomerUseCase(customerRepository);
 
-    const output = await customerUpdateUseCase.execute(input)
+    const output = await customerUpdateUseCase.execute(input);
 
-    expect(output).toEqual(input)
+    expect(output).toEqual(input);
   });
 });
